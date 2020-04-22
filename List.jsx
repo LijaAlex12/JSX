a99d90efe57a50e2ae31cb730dc75a63cc27d8cd
Lija Alex <48295613+LijaAlex12@users.noreply.github.com>
2020-04-22 18:33:53 +0530

    List.jsx

---
+++ List.jsx
@@ -0,0 +1,12 @@
+var React = require("react");
+
+function List() {
+  return (
+    <ul>
+      <li>Burger</li>
+      <li>Sandwhich</li>
+      <li>Pizza</li>
+    </ul>
+  );
+}
+export default List;
